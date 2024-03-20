4c70270c07a6dcfd7dfb20fa7c14baae83e463c7
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2024-03-19 23:45:16 -0500

    [Feature/Add] add the image in the file cats icons with the path of the image

--- lib/cats_icons.dart
+++ lib/cats_icons.dart
@@ -5,4 +5,5 @@ class PathBaseIcons {
 class CatsIcons {
   static const String imageCatSplash =
       "${PathBaseIcons.base}cat_for_screen_splash.png";
+  static const String notImage = "${PathBaseIcons.base}no_image.png";
 }
